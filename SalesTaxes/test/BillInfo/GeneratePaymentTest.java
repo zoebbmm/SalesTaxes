6d680ac3b540aea6f9cb8ff16955d4d4f147a160
zoebbmm <zhou.wei@sv.cmu.edu>
2013-12-30 02:43:23 -0500

    Test cases for core logic of classes defined in the 'BillInfo' package.

--- SalesTaxes/test/BillInfo/GeneratePaymentTest.java
+++ SalesTaxes/test/BillInfo/GeneratePaymentTest.java
@@ -1,7 +1,6 @@
 package BillInfo;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 import Products.*;
--- SalesTaxes/test/BillInfo/ReceiptTest.java
+++ SalesTaxes/test/BillInfo/ReceiptTest.java
@@ -1,9 +1,7 @@
 package BillInfo;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
 import Products.*;
