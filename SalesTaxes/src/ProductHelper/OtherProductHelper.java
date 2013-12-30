4b9c7680d1911242658d751e8a3c8c6efaca10bf
zoebbmm <zhou.wei@sv.cmu.edu>
2013-12-30 02:34:33 -0500

    Create 'Other Product'.

--- SalesTaxes/src/ProductHelper/OtherProductHelper.java
+++ SalesTaxes/src/ProductHelper/OtherProductHelper.java
@@ -2,7 +2,6 @@ package ProductHelper;
 
 import Products.OtherProduct;
 
-
 public class OtherProductHelper extends ProductHelper {
 	 public OtherProduct createProduct(String name, double price, boolean imported, int quantity) {
              return new OtherProduct(name, price, imported, quantity);
